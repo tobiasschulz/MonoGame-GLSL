8be3f78e4c8b654b1e62cef6dc13e9681bb911a6
Tobias Schulz <tobias@freehal.net>
2014-03-11 01:00:06 +0100

    update

--- MonoGame.GLSL/GLEffect.cs
+++ MonoGame.GLSL/GLEffect.cs
@@ -185,51 +185,5 @@ namespace MonoGame.GLSL
                 currentIndexBuffer = buffer;
             }
         }
-
-        void Apply(GLShader shader, IntPtr offset, int divisor = 0)
-        {
-            VertexDeclarationAttributeInfo attrInfo;
-            int shaderHash = shader.GetHashCode();
-            if (!shaderAttributeInfo.TryGetValue(shaderHash, out attrInfo))
-            {
-                // Get the vertex attribute info and cache it
-                attrInfo = new VertexDeclarationAttributeInfo(OpenGLDevice.Instance.MaxVertexAttributes);
-
-                foreach (var ve in _elements)
-                {
-                    var attributeLocation = shader.GetAttribLocation(ve.VertexElementUsage, ve.UsageIndex);
-                    // XNA appears to ignore usages it can't find a match for, so we will do the same
-                    if (attributeLocation >= 0)
-                    {
-                        attrInfo.Elements.Add(new VertexDeclarationAttributeInfo.Element()
-                                              {
-                            Offset = ve.Offset,
-                            AttributeLocation = attributeLocation,
-                            NumberOfElements = ve.VertexElementFormat.OpenGLNumberOfElements(),
-                            VertexAttribPointerType = ve.VertexElementFormat.OpenGLVertexAttribPointerType(),
-                            Normalized = ve.OpenGLVertexAttribNormalized(),
-                        });
-                        attrInfo.EnabledAttributes[attributeLocation] = true;
-                    }
-                }
-
-                shaderAttributeInfo.Add(shaderHash, attrInfo);
-            }
-
-            // Apply the vertex attribute info
-            foreach (var element in attrInfo.Elements)
-            {
-                OpenGLDevice.Instance.AttributeEnabled[element.AttributeLocation] = true;
-                OpenGLDevice.Instance.Attributes[element.AttributeLocation].Divisor.Set(divisor);
-                OpenGLDevice.Instance.VertexAttribPointer(
-                    element.AttributeLocation,
-                    element.NumberOfElements,
-                    element.VertexAttribPointerType,
-                    element.Normalized,
-                    VertexStride,
-                    (IntPtr) (offset.ToInt64() + element.Offset)
-                    );
-            }
-        }
     }
 }
